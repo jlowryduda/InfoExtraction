7648ecc0e5f7f54ceb62c0e99d3cb630e845832c
Sara Libenson <slibenson@observation.lumi>
2015-03-02 10:51:36 -0500

    Move 'read_tagged_file' over to extract_features.py, add pos_match feature

--- PA2/extract_features.py
+++ PA2/extract_features.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 def read_from_file(filename):
     with open(filename, 'r') as infile:
@@ -7,6 +8,43 @@ def read_from_file(filename):
     lines = [line.split() for line in lines]
     return lines
 
+def read_tagged_file(filename):
+    """
+    Reads in an original pos-tagged file and converts it to a list of
+    sentences, each of which contains a list of (token, tag) pairs.  The
+    resulting data structure can be indexed into with the indices from the
+    train/dev/test data.
+    """
+    path = os.getcwd() + "/data/postagged-files/"
+    with open(path + filename + '.raw.pos', 'r') as infile:
+        raw_string = infile.read()
+    sents = raw_string.split('\n')
+    # Get rid of empty sentences:
+    sents = [sent.split() for sent in sents if len(sent) > 0]
+    for i, sent in enumerate(sents):
+        for j, token in enumerate(sent):
+            sent[j] = tuple(token.rsplit('_', 1))
+        sents[i] = sent
+    return sents
+
+def pos_match(line):
+    """
+    Given a line of train/dev/test data, creates the document data structure
+    for the appropriate file and indexes into it at the indices given in the
+    line to get the POS-tags for each entity in the coreference pair. Returns
+    true if intersection of two sets of POS-tags is non-empty. Otherwise,
+    returns false if there are no POS-tags in common.
+    """
+    sents = read_tagged_file(line[0])
+    span1 = sents[int(line[1])][int(line[2]):int(line[3])]
+    span2 = sents[int(line[6])][int(line[7]):int(line[8])]
+    tags1 = set([item[1] for item in span1])
+    tags2 = set([item[1] for item in span2])
+    if len(set.intersection(tags1, tags2)) > 0:
+        return "pos_match=1"
+    else:
+        return "pos_match=0"
+
 def get_distance(line):
     """ Returns the distance between sentences """
     return "distance=" + str(abs(int(line[1]) - int(line[6])))
@@ -14,22 +52,19 @@ def get_distance(line):
 def is_contained(line):
     return "is_contained=" + str(line[5] in line[10])
 
-
 def get_label(line):
     return line[-1]
 
 def entity_types_match(line):
     return "types_match=" + str(line[4] == line[9])
 
-
 def extract_features(lines, train=False):
     features = []
     for line in lines:
-        #if line[11] == "yes":
-            #print line
         f_list = [get_distance(line),
                   is_contained(line),
-                  entity_types_match(line)]
+                  entity_types_match(line),
+                  pos_match(line)]
         if train:
             f_list.insert(0, get_label(line))
         features.append(f_list)
@@ -44,8 +79,8 @@ def write_to_file(filename, features):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print "Specify an input filename, an output filename, and"
-        print "either 'train' or 'test' depending on datatype"
+        print("Specify an input filename, an output filename, and")
+        print("either 'train' or 'test' depending on datatype")
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
@@ -56,4 +91,3 @@ if __name__ == "__main__":
         lines = read_from_file(input_file)
         lines = extract_features(lines, train)
         write_to_file(output_file, lines)
-
