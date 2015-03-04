9af3a629f1c7ad79aa3405c7a742882d8d9f87cf
slibs63 <sara.libenson@gmail.com>
2015-03-04 10:35:37 -0500

    Add save_jsons.py and update extract_features.py to use new json structures.

--- PA2/extract_features.py
+++ PA2/extract_features.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 def read_from_file(filename):
     with open(filename, 'r') as infile:
@@ -8,34 +9,15 @@ def read_from_file(filename):
     lines = [line.split() for line in lines]
     return lines
 
-def read_tagged_file(filename):
-    """
-    Reads in an original pos-tagged file and converts it to a list of
-    sentences, each of which contains a list of (token, tag) pairs.  The
-    resulting data structure can be indexed into with the indices from the
-    train/dev/test data.
-    """
-    path = os.getcwd() + "/data/postagged-files/"
-    with open(path + filename + '.raw.pos', 'r') as infile:
-        raw_string = infile.read()
-    sents = raw_string.split('\n')
-    # Get rid of empty sentences:
-    sents = [sent.split() for sent in sents if len(sent) > 0]
-    for i, sent in enumerate(sents):
-        for j, token in enumerate(sent):
-            sent[j] = tuple(token.rsplit('_', 1))
-        sents[i] = sent
-    return sents
 
-def pos_match(line):
+def pos_match(line, sents):
     """
-    Given a line of train/dev/test data, creates the document data structure
-    for the appropriate file and indexes into it at the indices given in the
+    Given a line of train/dev/test data, and the document data structure
+    for the appropriate file, it indexes into it at the indices given in the
     line to get the POS-tags for each entity in the coreference pair. Returns
     true if intersection of two sets of POS-tags is non-empty. Otherwise,
     returns false if there are no POS-tags in common.
     """
-    sents = read_tagged_file(line[0])
     span1 = sents[int(line[1])][int(line[2]):int(line[3])]
     span2 = sents[int(line[6])][int(line[7]):int(line[8])]
     tags1 = set([item[1] for item in span1])
@@ -45,30 +27,73 @@ def pos_match(line):
     else:
         return "pos_match=0"
 
+
+def gender_match(line, sents):
+	"""
+	Given a line containing a potential coreference pair, and a sentence data
+	structure, determine whether or not the pair has the same gender.
+	"""
+	# Assume that the first name part in any given span of tokens will be
+	# most indicative of gender (because it's most likely the first name):
+	names = [sents[int(line[1])][int(line[2])][0],
+			 sents[int(line[6])][int(line[7])][0]]
+	ents = [line[4], line[9]]
+	genders = ['N', 'N'] # Default to neutral gender
+	with open('names_genders.json', 'r') as infile:
+		gender_dict = json.load(infile)
+
+	# If the entity is a person, check to see if the person's gender is in the
+	# gender dictionary as either male or female, and if so, reassign
+	for i in range(len(names)):
+		if ents[i] == 'PER':
+			if names[i] in gender_dict:
+				if gender_dict[names[i]] == 'male':
+					genders[i] = 'M'
+				elif gender_dict[names[i]] == 'female':
+					genders[i] = 'F'
+
+	if genders[0] == genders[1]:
+		return "gender_match=1"
+	else:
+		return "gender_match=0"
+
+
 def get_distance(line):
     """ Returns the distance between sentences """
     return "distance=" + str(abs(int(line[1]) - int(line[6])))
 
+
 def is_contained(line):
     return "is_contained=" + str(line[5] in line[10])
+
 
 def get_label(line):
     return line[-1]
 
+
 def entity_types_match(line):
     return "types_match=" + str(line[4] == line[9])
 
+
 def extract_features(lines, train=False):
     features = []
+    json_path = os.getcwd() + '/jsons/'
+    current_file = None
     for line in lines:
+    	if line[0] != current_file:
+    		current_file = line[0]
+    		with open(json_path + current_file + '.raw.json', 'r') as infile:
+    			sents = json.load(infile)
         f_list = [get_distance(line),
                   is_contained(line),
                   entity_types_match(line),
-                  pos_match(line)]
+                  gender_match(line, sents)
+                  pos_match(line, sents)]
         if train:
             f_list.insert(0, get_label(line))
         features.append(f_list)
     return features
+
 
 def write_to_file(filename, features):
     lines = [' '.join(f) for f in features]
@@ -76,6 +101,7 @@ def write_to_file(filename, features):
         for line in lines:
             outfile.write(line)
             outfile.write('\n')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
