82451755f2f59b481ba618c63853a44c24180d9d
slibs63 <sara.libenson@gmail.com>
2015-03-07 09:22:09 -0500

    Initial attempt at appositive feature code.

--- PA2/extract_features.py
+++ PA2/extract_features.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+from nltk.tree import Tree
 
 def read_from_file(filename):
     with open(filename, 'r') as infile:
@@ -29,33 +30,33 @@ def pos_match(line, sents):
 
 
 def gender_match(line, sents):
-	"""
-	Given a line containing a potential coreference pair, and a sentence data
-	structure, determine whether or not the pair has the same gender.
-	"""
-	# Assume that the first name part in any given span of tokens will be
-	# most indicative of gender (because it's most likely the first name):
-	names = [sents[int(line[1])][int(line[2])][0],
-			 sents[int(line[6])][int(line[7])][0]]
-	ents = [line[4], line[9]]
-	genders = ['N', 'N'] # Default to neutral gender
-	with open('names_genders.json', 'r') as infile:
-		gender_dict = json.load(infile)
+    """
+    Given a line containing a potential coreference pair, and a sentence data
+    structure, determine whether or not the pair has the same gender.
+    """
+    # Assume that the first name part in any given span of tokens will be
+    # most indicative of gender (because it's most likely the first name):
+    names = [sents[int(line[1])][int(line[2])][0],
+             sents[int(line[6])][int(line[7])][0]]
+    ents = [line[4], line[9]]
+    genders = ['N', 'N'] # Default to neutral gender
+    with open('names_genders.json', 'r') as infile:
+        gender_dict = json.load(infile)
 
-	# If the entity is a person, check to see if the person's gender is in the
-# gender dictionary as either male or female, and if so, reassign
-	for i in range(len(names)):
-		if ents[i] == 'PER':
-			if names[i] in gender_dict:
-				if gender_dict[names[i]] == 'male':
-					genders[i] = 'M'
-				elif gender_dict[names[i]] == 'female':
-					genders[i] = 'F'
+    # If the entity is a person, check to see if the person's gender is in the
+    # gender dictionary as either male or female, and if so, reassign
+    for i in range(len(names)):
+        if ents[i] == 'PER':
+            if names[i] in gender_dict:
+                if gender_dict[names[i]] == 'male':
+                    genders[i] = 'M'
+                elif gender_dict[names[i]] == 'female':
+                    genders[i] = 'F'
 
-	if genders[0] == genders[1]:
-		return "gender_match=1"
-	else:
-		return "gender_match=0"
+    if genders[0] == genders[1]:
+        return "gender_match=1"
+    else:
+        return "gender_match=0"
 
 
 def get_distance(line):
@@ -88,25 +89,53 @@ def both_proper_names(line, sents):
     span2 = sents[int(line[6])][int(line[7]):int(line[8])]
     tags1 = set([item[1] for item in span1])
     tags2 = set([item[1] for item in span2])
-    return "both_proper_names=" + str(any(item.startswith("NNP") for item in tags1) and any(item.startswith("NNP") for item in tags))
+    return "both_proper_names=" + str(any(item.startswith("NNP") for item in tags1) and any(item.startswith("NNP") for item in tags2))
+
+def is_appositive(line, constituents):
+    if line[1] == line[6]:
+        tree = constituents[int(line[1])]
+        start = int(line[2])
+        end = int(line[7])
+        subtree = tree[tree.treeposition_spanning_leaves(start, end)]
+
+        if len(subtree) >= 4:
+            if (subtree.label() == 'NP' and     # Rooted at NP
+                subtree[0].label() == 'NP' and  # First subtree is an NP
+                subtree[1].label() == ',' and   # Second subtree is a comma
+                subtree[3].label() == ','):     # Fourth subtree is a comma
+                # We may very well be dealing with an appositive,
+                # but more exploration of subtree[2] is needed.
+                # print(subtree)
+                return "is_appositive=1"
+    else:
+        return "is_appositive=0"
 
 def extract_features(lines, train=False):
     features = []
     json_path = os.getcwd() + '/jsons/'
+    parse_path = os.getcwd() + '/parsed/'
     current_file = None
     for line in lines:
     	if line[0] != current_file:
-    		current_file = line[0]
-    		with open(json_path + current_file + '.raw.json', 'r') as infile:
-    			sents = json.load(infile)
+            current_file = line[0]
+            with open(json_path + current_file + '.raw.json', 'r') as infile:
+                sents = json.load(infile)
+            with open(parse_path + current_file + '.raw.pos.parsed', 'r') as infile:
+                sents = infile.read()
+                sents = sents.split('\n\n')
+                sents = [sent for sent in sents if len(sent) > 0]
+                # Skip over dependency trees for now
+                constituents = [s for i, s in enumerate(sents) if i % 2 == 0]
+                constituents = [Tree.fromstring(c) for c in constituents]
         f_list = [get_distance(line),
                   is_contained(line),
                   entity_types_match(line),
-                  gender_match(line, sents)
+                  gender_match(line, sents),
                   pos_match(line, sents),
-                  antecendent_pronoun(line),
+                  antecedent_pronoun(line),
                   anaphor_pronoun(line),
-                  both_proper_names(line, sents)]
+                  both_proper_names(line, sents),
+                  is_appositive(line, constituents)]
         if train:
             f_list.insert(0, get_label(line))
         features.append(f_list)
