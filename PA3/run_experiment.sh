0a03c3d67fbe96f3d54ffde2334117243f8849b5
Sara Libenson <slibenson@observation.lumi>
2015-03-11 13:35:13 -0400

    Copy pertinent files to run the experiments over from PA2

---
+++ PA3/run_experiment.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Specify paths to train and dev files below
+
+EXPERIMENT_DIR=/home/g/grad/jduda/ie
+MALLET=/home/j/clp/chinese/bin/mallet-maxent-classifier.sh
+
+# train the classifier
+$MALLET -train \
+	-model=$EXPERIMENT_DIR/model \
+	-gold=$EXPERIMENT_DIR/train.features.labeled
+
+# test model
+$MALLET -classify  \
+	-model=$EXPERIMENT_DIR/model \
+	-input=$EXPERIMENT_DIR/dev.features.nolabel > $EXPERIMENT_DIR/dev.features.tagged
+
+# evaluate
+python relation-evaluator.py dev.features.labeled dev.features.tagged
