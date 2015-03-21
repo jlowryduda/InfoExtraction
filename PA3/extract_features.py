ae99e961d25c0b398fb7bfde77cf6f9612610723
slibs63 <sara.libenson@gmail.com>
2015-03-20 22:07:55 -0400

    Add capability of writing tree to feature file

--- PA3/extract_features.py
+++ PA3/extract_features.py
@@ -55,6 +55,20 @@ def interceding_in(line, sents):
     pass
 
 
+def get_tree(line, constituents, attribute):
+    if int(line[2]) == int(line[8]):
+        tree = constituents[int(line[2])]
+        subtree = tree.treeposition_spanning_leaves(int(line[3]), int(line[10]))
+        subtree_string = tree[subtree].pprint()
+        subtree_lines = subtree_string.splitlines()
+        tree = [t.strip() for t in subtree_lines]
+        tree = ' '.join(tree)
+        tree = tree.replace(') (', ')(')
+        return tree
+    else:
+        return 'None'
+    
+
 def extract_features(lines):
     """
     Given lines of data, extracts features.
@@ -62,15 +76,25 @@ def extract_features(lines):
     features = []
     curr_file = None
     j_path = os.getcwd() + '/data/jsons/'
+    p_path = os.getcwd() + '/data/parsed/'
     for line in lines:
-    	if line[1] != curr_file:
+        if line[1] != curr_file:
             curr_file = line[1]
             print(curr_file)
             with open(j_path + curr_file + '.json', 'r') as infile:
                 sents = json.load(infile)
+            with open(p_path + curr_file + '.parsed', 'r') as infile:
+                parses = infile.read()
+                parses = parses.split('\n\n')
+                parses = [sent for sent in parses if len(sent) > 0]
+                constituents = [s for i, s in enumerate(parses) if i % 3 == 1]
+                dependencies = [s.split('\n') for i, s in enumerate(parses)
+                                if i % 3 == 2]
+                constituents = [Tree.fromstring(c) for c in constituents]
         f_list = [get_label(line),
-                 entity_type_pair(line),
-                 interceding_in(line, sents)]
+                  '|BT|',
+                  get_tree(line, constituents, 'leaves'),
+                  '|ET|']
         features.append([f for f in f_list if f is not None])
     return features
 
@@ -85,7 +109,9 @@ def write_to_file(filename, features, label=None, train=False):
     with open(os.getcwd() + '/data/' + filename + '.labeled', 'w') as outfile:
         for feature in features:
             if label and (feature[0] != label):
-                feature[0] = 'no_rel'
+                feature[0] = '-1'
+            else:
+                feature[0] = '1'
             outfile.write(' '.join(feature))
             outfile.write('\n')
     if not train:
@@ -103,10 +129,24 @@ if __name__ == "__main__":
     else:
         file_name = sys.argv[1]
         label_name = sys.argv[2]
-        if sys.argv[3] == 'train':
+        if file_type == 'train':
             train = True
         else:
             train = False
         lines = read_from_file(file_name)
         features = extract_features(lines)
         write_to_file(file_name, features, label_name, train)
+
+"""
+if __name__ == "__main__":
+    file_name = 'dev' #sys.argv[1]
+    label_name = 'PHYS.Located' #sys.argv[2]
+    file_type = 'train'
+    if file_type == 'train':
+        train = True
+    else:
+        train = False
+    lines = read_from_file(file_name)
+    features = extract_features(lines)
+    write_to_file(file_name, features, label_name, train)
+"""
