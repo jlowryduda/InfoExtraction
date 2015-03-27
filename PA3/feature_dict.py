553f041b1dd6802e3780b605aaa448e29cb201a1
temblorosa <amandalynne.p@gmail.com>
2015-03-27 11:33:37 -0400

    fixed up feature_dict

--- PA3/feature_dict.py
+++ PA3/feature_dict.py
@@ -5,14 +5,8 @@ class FeatureDict:
     Map features to indices. Initialize with a list of feature names.
     Heavily inspired by Plotnick's CodeBook class.
     """
-
-    def __init__(self, vocabulary_path, feature_set):
-        self.vocabulary_path = vocabulary_path
-        self.feature_set = feature_set
-        self.feature_dict =\
-            dict((feature, index) for index, feature in enumerate(feature_set))
-        self.read_vocabulary()
-
+    def __init__(self):
+        self.feature_dict = {}
 
     def __contains__(self, item):
         return item in self.feature_dict
@@ -23,10 +17,6 @@ class FeatureDict:
     def __len__(self):
         return len(self.feature_dict)
 
-    def read_vocabulary(self):
-        with open(self.vocabulary_path, "r") as vocabulary_file:
-            self.vocab = vocabulary_file.read().splitlines()
-
     def __getitem__(self, feature):
         """Return the index for given feature."""
         return self.feature_dict[feature]
@@ -36,9 +26,4 @@ class FeatureDict:
         if item not in self:
             index = len(self)
             self.feature_dict[item] = index
-        return item       
-    
-    def get(self, item, default=None):
-        """Return index associated with item."""
-        return self.feature_dict.get(item, default)
- 
+        return len(self.feature_dict)       
